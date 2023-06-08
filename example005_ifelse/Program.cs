4a45a84e0f16533e53735127bcb71b2e2e882236
Elena <elena_nn@list.ru>
2023-06-08 18:57:39 +0300

    example005_ifelse

---
+++ example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+String username = Console.ReadLine();
+if(username.ToLower() =="елена")
+{
+  Console.WriteLine("Ура, это же Елена!");  
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.WriteLine("!");
+}
