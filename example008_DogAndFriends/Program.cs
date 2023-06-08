98d20ed25acabba4281fd2ae2e900a7607567aea
Elena <elena_nn@list.ru>
2023-06-08 21:54:01 +0300

    friend

---
+++ example008_DogAndFriends/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0;
+double distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+friend = 2;
+
+while(distance>10)
+{
+    if(friend = 1)
+    {
+       time = distanse/(firstFriendSpeed + dogSpeed);
+       friend = 2;
+    }
+    else
+    {
+        time = distanse/(secondFrienSpeed + dogSpeed);
+        friend = 1;
+    }
+    distanse = distanse - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.WriteLine(count);
